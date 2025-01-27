8a2de3e42e5682fe65302159fa2fdfa94c75f231
Marcusenok <mark-marchenkov@mail.ru>
2025-01-27 14:30:03 +0300

    update enemy

--- Scenes/Enemy/Defalt_enemy.gd
+++ Scenes/Enemy/Defalt_enemy.gd
@@ -12,13 +12,5 @@ func deal_damage() -> void:
 	else:
 		hp_boss_label.text = str(hp_boss)
 
-func _process(delta: float) -> void:
-	pass
-	#position += boss_speed * delta
-	#length_boss_path -= abs(boss_speed.x) * delta
-	#if length_boss_path <= 0:
-		#length_boss_path = 300
-		#boss_speed *= -1
-
 func enemy():
 	pass
