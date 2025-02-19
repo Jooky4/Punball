b92badbc9e4957a239207858902bc3b43b5023a2
AskKode <askod192@gmail.com>
2025-02-19 15:24:08 +0300

    Эффекты на лазер

--- Scenes/For skills/laser_line.gd
+++ Scenes/For skills/laser_line.gd
@@ -2,9 +2,13 @@ extends Sprite2D
 
 func show_line(rotate):
 	if rotate == 1:
-		create_tween().tween_property(self, "scale", Vector2(1.25, 1), 0.145)
+		var tween = create_tween().tween_property(self, "scale", Vector2(1.25, 0.75), 0.14)
+		tween.set_trans(Tween.TRANS_SINE) #  Тип перехода (синусоидальный - хороший выбор для плавного)
+		tween.set_ease(Tween.EASE_IN_OUT) #  Плавность (ускорение в начале, замедление в конце)
 		self.rotation_degrees = 90
 	elif rotate == 0:
-		create_tween().tween_property(self, "scale", Vector2(1, 1), 0.145)
-	await get_tree().create_timer(0.18).timeout
+		var tween = create_tween().tween_property(self, "scale", Vector2(1, 0.75), 0.14)
+		tween.set_trans(Tween.TRANS_SINE) # Тип перехода
+		tween.set_ease(Tween.EASE_IN_OUT) # Плавность
+	await get_tree().create_timer(0.2).timeout
 	queue_free()
