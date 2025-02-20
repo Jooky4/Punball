5b4995028c0a2521d8b09ce034afe298fb3e3fcd
AskKode <askod192@gmail.com>
2025-02-20 15:04:34 +0300

    Анимация для шипов

--- Scenes/For skills/thorns.gd
+++ Scenes/For skills/thorns.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 func damage_enemy(enemy) -> void:
 	enemy.deal_damage(600 * ElementsManager.technologies_modifier, ElementsManager.color_elements["TECHNOLOGIES"])
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(1).timeout
 	queue_free()
