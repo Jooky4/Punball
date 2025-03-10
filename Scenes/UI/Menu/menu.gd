765507ae8b7d0842767a1309ecfaf9646c8392eb
Marcusenok <mark-marchenkov@mail.ru>
2025-03-11 00:09:06 +0300

    update main menu

--- Scenes/UI/Menu/menu.gd
+++ Scenes/UI/Menu/menu.gd
@@ -1,6 +1,12 @@
 extends Control
 
+@onready var shop_UI = $Shop
+@onready var main_menu_UI = $Main_menu
+@onready var talents_UI = $Talents
+
 func _ready() -> void:
+	main_menu_UI.visible = true
+	shop_UI.visible = false
 	Engine.max_fps = 10000
 	YandexSDK.init_game()
 	YandexSDK.game_ready()
@@ -9,10 +15,14 @@ func _on_button_pressed() -> void:
 	AudioManager.click()
 	get_tree().change_scene_to_file("res://Scenes/Levels/first_level.tscn")
 
-func _on_button_2_pressed() -> void:
+func _on_shop_button_pressed() -> void:
 	AudioManager.click()
-	YandexSDK.show_interstitial_ad()
+	shop_UI.visible = true
+	main_menu_UI.visible = false
+	talents_UI.visible = false
 
-func _on_button_3_pressed() -> void:
+func _on_talesnts_button_pressed() -> void:
 	AudioManager.click()
-	YandexSDK.show_rewarded_ad()
+	talents_UI.visible = true
+	main_menu_UI.visible = false
+	shop_UI.visible = false
