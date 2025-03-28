c658c46d071764545798da5b98257e29e0043e6e
AskKode <askod192@gmail.com>
2025-03-28 10:35:56 +0300

    Правки в сундук и эффекты на него

--- Scenes/UI/Menu/сhest.gd
+++ Scenes/UI/Menu/сhest.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var label_chest = $Label
 @onready var texture_close = $Close
 @onready var texture_open = $Open
+@onready var texture_epty = $Empty #добавил Дима
 @onready var button_open_chest = $Open_chest
 
 var location_for_open : int = 0
@@ -23,6 +24,8 @@ func open_chest() -> void:
 	texture_open.visible = true
 
 func _on_open_chest_pressed() -> void:
+	texture_epty.visible = true #добавил Дима
+	texture_open.visible = false #добавил Дима
 	emit_signal("chest_opened")
 
 func can_open_or_not(arr) -> void:
