1e255e517871229f2bda83d1ec987691aa451e01
Marcusenok <mark-marchenkov@mail.ru>
2025-04-02 15:05:13 +0300

    update game

--- Scenes/Global/level_manager.gd
+++ Scenes/Global/level_manager.gd
@@ -456,8 +456,8 @@ func can_boss_spawn() -> bool:
 	return true
 
 func ball_explosion(enemy, damage_ball, color_ball, create_sound : bool = false) -> void:
-	var x
-	var y
+	var x = 0
+	var y = 0
 	for i in range(first_level_links_on_objects.size()):
 		for j in range(first_level_links_on_objects[i].size()):
 			if first_level_links_on_objects[i][j] != null:
@@ -465,27 +465,28 @@ func ball_explosion(enemy, damage_ball, color_ball, create_sound : bool = false)
 					x = i
 					y = j
 					break
-	for dx in range(-1, 2):
-		for dy in range(-1, 2):
-			var target_x = x + dx
-			var target_y = y + dy
-			if target_x >= 0 and target_x < 8 and target_y >= 0 and target_y < 6:
-				if first_level_links_on_objects[target_x][target_y] != null:
-					if first_level_links_on_objects[target_x][target_y].has_method("enemy"):
-						if first_level_links_on_objects[target_x][target_y].alive:
-							if color_ball == ElementsManager.color_elements["POISON"]:
-								first_level_links_on_objects[target_x][target_y].poisoning()
-								AudioManager.bomb_sound()
-							elif color_ball == ElementsManager.color_elements["FIRE"]:
-								first_level_links_on_objects[target_x][target_y].deal_bomb_damage(damage_ball, color_ball)
-								if create_sound:
+	if x != 0 and y != 0:
+		for dx in range(-1, 2):
+			for dy in range(-1, 2):
+				var target_x = x + dx
+				var target_y = y + dy
+				if target_x >= 0 and target_x < 8 and target_y >= 0 and target_y < 6:
+					if first_level_links_on_objects[target_x][target_y] != null:
+						if first_level_links_on_objects[target_x][target_y].has_method("enemy"):
+							if first_level_links_on_objects[target_x][target_y].alive:
+								if color_ball == ElementsManager.color_elements["POISON"]:
+									first_level_links_on_objects[target_x][target_y].poisoning()
 									AudioManager.bomb_sound()
-							elif color_ball == ElementsManager.color_elements["FROST"]:
-								first_level_links_on_objects[target_x][target_y].deal_freezing_damage(damage_ball, color_ball)
-								if create_sound:
-									AudioManager.freezing_bomb_sound()
-							combo_count += 1 
-							check_count_combo(first_level_links_on_objects[target_x][target_y])
+								elif color_ball == ElementsManager.color_elements["FIRE"]:
+									first_level_links_on_objects[target_x][target_y].deal_bomb_damage(damage_ball, color_ball)
+									if create_sound:
+										AudioManager.bomb_sound()
+								elif color_ball == ElementsManager.color_elements["FROST"]:
+									first_level_links_on_objects[target_x][target_y].deal_freezing_damage(damage_ball, color_ball)
+									if create_sound:
+										AudioManager.freezing_bomb_sound()
+								combo_count += 1 
+								check_count_combo(first_level_links_on_objects[target_x][target_y])
 
 func lighthing_ball_damage(enemy, damage_ball, color_ball, create_sound : bool = false) -> void:
 	var enemy_arr = find_all_enemys()
