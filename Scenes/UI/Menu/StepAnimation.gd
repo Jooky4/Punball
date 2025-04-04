04f1d2da932437b56cb7fa7cf89aab54adc297af
AskKode <askod192@gmail.com>
2025-04-04 15:11:35 +0300

    Анимация для пальцев

---
+++ Scenes/UI/Menu/StepAnimation.gd
@@ -0,0 +1,24 @@
+extends TextureRect
+
+# Скорость анимации (продолжительность одного цикла в секундах)
+@export var animation_duration: float = 1.0
+
+# Масштаб увеличения объекта
+@export var max_scale: float = 1.1
+
+# Ссылка на Tween
+var tween: Tween
+
+func _ready():
+	# Создаем Tween
+	tween = create_tween()
+	
+	# Настройка бесконечной анимации
+	tween.tween_property(self, "scale", Vector2(max_scale, max_scale), animation_duration / 2)
+	tween.tween_property(self, "scale", Vector2(1, 1), animation_duration / 2)
+	tween.set_loops()  # Бесконечный цикл
+
+func _exit_tree():
+	# Очищаем Tween при выходе из дерева сцены
+	if tween:
+		tween.kill()
