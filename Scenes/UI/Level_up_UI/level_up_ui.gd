21c023a64ab31f2aad95b5fd88c449383b456f65
Krapivin Stepan <xevinbox@yandex.ru>
2025-05-06 13:02:34 +0300

    убрал пустые ячейки из level_up_ui

--- Scenes/UI/Level_up_UI/level_up_ui.gd
+++ Scenes/UI/Level_up_UI/level_up_ui.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var level_label = $Player_level/Player_level_label
 @onready var coins_label = $GridContainer/TextureRect3/Coins_label
 
+
 func level_up(count_level : float) -> void:
 	self.visible = true
 	coins_label.text = "x" + str((count_level / 4) * 1000)
@@ -11,6 +12,7 @@ func level_up(count_level : float) -> void:
 	PlayerIndicatorsManager.update_coins_count((count_level / 4) * 1000)
 	level_label.text = str(count_level)
 	level_up_sound.play()
+
 
 func _on_continue_pressed() -> void:
 	self.visible = false
