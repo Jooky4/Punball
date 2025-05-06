7a924f7f780bca962c3664162950b572513aa574
Krapivin Stepan <xevinbox@yandex.ru>
2025-05-06 17:53:10 +0300

    int вместо float при выводе уровня и монет в level_up_ui

--- Scenes/UI/Level_up_UI/level_up_ui.gd
+++ Scenes/UI/Level_up_UI/level_up_ui.gd
@@ -5,11 +5,12 @@ extends Control
 @onready var coins_label = $GridContainer/TextureRect3/Coins_label
 
 
-func level_up(count_level : float) -> void:
+func level_up(count_level: int) -> void:
 	self.visible = true
-	coins_label.text = "x" + str((count_level / 4) * 1000)
+	var coins_count: int = (count_level / 4.0) * 1000
+	coins_label.text = "x" + str(coins_count)
 	PlayerIndicatorsManager.update_crystal_count(+50)
-	PlayerIndicatorsManager.update_coins_count((count_level / 4) * 1000)
+	PlayerIndicatorsManager.update_coins_count(coins_count)
 	level_label.text = str(count_level)
 	level_up_sound.play()
 
