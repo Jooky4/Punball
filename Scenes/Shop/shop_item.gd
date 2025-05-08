eccb681bb0db8b867c9e278d0fb0e3f7346df4b4
Krapivin Stepan <xevinbox@yandex.ru>
2025-05-08 11:27:40 +0300

    создал компонент shop_item

---
+++ Scenes/Shop/shop_item.gd
@@ -0,0 +1,35 @@
+@tool
+extends TextureButton
+
+@export_enum("coins", "crystal") var type: int
+@export var price: int
+@export var count: int
+
+@export var item_texture: Texture2D
+
+@export var coins_bg_texture: Texture2D
+@export var crystal_bg_texture: Texture2D
+
+@export var coin_texture: Texture2D
+@export var crystal_texture: Texture2D
+
+
+@onready var item_image: TextureRect = $TextureRect
+@onready var item_count_label: Label = %CountLabel
+@onready var price_label: Label = %PriceLabel
+@onready var count_image: TextureRect = $HBoxContainer2/TextureRect
+
+
+func _ready() -> void:
+	var current_bg_texture: Texture2D = coins_bg_texture
+	var current_count_texture: Texture2D = coin_texture
+
+	if type == 1:
+		current_bg_texture = crystal_bg_texture
+		current_count_texture = crystal_texture
+
+	texture_normal = current_bg_texture
+	item_image.texture = item_texture
+	price_label.text = str(price)
+	item_count_label.text = str(count)
+	count_image.texture = current_count_texture
